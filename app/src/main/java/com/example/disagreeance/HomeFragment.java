97d8b43dcc9587316a54f74cbf1815bd376f23e2
janifkader <janif.kader@gmail.com>
2025-07-01 21:14:53 -0400

    fix: Fixed authentication to work with new database rules.

--- app/src/main/java/com/example/disagreeance/HomeFragment.java
+++ app/src/main/java/com/example/disagreeance/HomeFragment.java
@@ -5,13 +5,14 @@ import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeFragment extends Fragment {
 
@@ -28,6 +29,10 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.signInAnonymously()
+                .addOnCompleteListener(task -> {
+                });
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         view.findViewById(R.id.ruleButton).setOnClickListener(new View.OnClickListener(){
             @Override
